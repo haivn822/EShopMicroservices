028ec1d0bc33a6d130cba14a09234fa5d3723709
haivn822 <nghai93@gmail.com>
2024-12-26 22:34:09 +0700

    add get product handler

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,27 +1,23 @@
-﻿
+﻿namespace Catalog.API.Products.CreateProduct;
 
-namespace Catalog.API.Products.CreateProduct
+public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
+public record CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint : ICarterModule
 {
-
-    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal price);
-
-    public record CreateProductResponse(Guid Id);
-
-    public class CreateProductEndpoint : ICarterModule
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
-        public void AddRoutes(IEndpointRouteBuilder app)
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
         {
-            app.Map("/products", async (CreateProductRequest request, ISender sender) =>
-            {
-                var command = request.Adapt<CreateProductCommand>();
-                var result = await sender.Send(command);
-                var response = result.Adapt<CreateProductResponse>();
-                return Results.Created($"/products/{response.Id}", response);
-            }).WithName("CreateProduct")
-            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
-            .ProducesProblem(StatusCodes.Status400BadRequest)
-            .WithSummary("Create Product")
-            .WithDescription("Create Product");
-        }
+            var command = request.Adapt<CreateProductCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<CreateProductResponse>();
+            return Results.Created($"/products/{response.Id}", response);
+        }).WithName("CreateProduct")
+     .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+     .ProducesProblem(StatusCodes.Status400BadRequest)
+     .WithSummary("Create Product")
+     .WithDescription("Create Product");
     }
 }
