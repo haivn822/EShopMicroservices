a94aba55ada59b14b14b22bd92024c77e102d74f
haivn822 <nghai93@gmail.com>
2025-05-05 21:49:11 +0700

    DependencyInjection

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,15 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //add services to the container
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+
 
 var app = builder.Build();
 
